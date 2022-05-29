fd48928bff8babe2d5048109f4e0bda0a5715192
Azar Barfipour <azar.barfipour@gmail.com>
2022-05-29 15:43:02 -0700

    solve mobile version for works part

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,7 +1,6 @@
 document.querySelector(".nav__links").addEventListener("click", function (e) {
   e.preventDefault();
   if (e.target.classList.contains("nav-link")) {
-    console.log(e.target.getBoundingClientRect());
     const id = e.target.getAttribute("href");
     document
       .querySelector(id)
